07d4695704ade24be6e8977339fdebd594363cb0
simbot <anirban.robotics@gmail.com>
2020-09-15 09:23:51 -0400

    Linked List class updated

--- LinkedList/BuildLinkedList.cpp
+++ LinkedList/BuildLinkedList.cpp
@@ -19,7 +19,7 @@ public:
     head = nullptr;
   }
 
-  void BuildLinkdList(std::vector<int>& vec) {
+  void Build(std::vector<int>& vec) {
     head = new Node(vec[0]);
     Node* current = head;
     for (size_t i = 1; i < vec.size(); i++) {
@@ -29,13 +29,35 @@ public:
     std::cout << "Linked List building completed" << std::endl;
   }
 
-  void TraverseLinkedList() {
+  void Traverse() {
     Node* current = this->head;
     while (current != nullptr) {
       std::cout << current->value << "->";
       current = current->next;
     }
     std::cout << "nullptr" << std::endl;
+  }
+
+  void Reverse() {
+    Node* previous = new Node(head->value);
+    Node* current = head->next;
+    delete head;
+
+    Node* oldCurrent;
+    while (current != nullptr) {
+      Node* node = new Node(current->value);
+      node->next = previous;
+      previous = node;
+
+      oldCurrent = current;
+      current = current->next;
+
+      // we are creating new nodes for new list
+      // and deleting nodes of the old list
+      delete oldCurrent;
+    }
+
+    head = previous;
   }
 
   void delNodes(Node* node) {
@@ -56,8 +78,14 @@ int main(int argc, char** argv) {
   std::vector<int> vec = {1,2,3,4,5,6};
 
   LinkedList* ll = new LinkedList();
-  ll->BuildLinkdList(vec);
-  ll->TraverseLinkedList();
+
+  // Build and traverse the linked list
+  ll->Build(vec);
+  ll->Traverse();
+
+  // Reverse and traverse the linked list
+  ll->Reverse();
+  ll->Traverse();
 
   delete ll;
 
